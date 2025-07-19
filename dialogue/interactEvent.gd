5606571b49f89d3cc5123eb9bf8900a0c7be67be
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2025-07-19 15:36:05 +0200

    changed name to interactevent

--- dialogue/interactEvent.gd
+++ dialogue/interactEvent.gd
@@ -1,7 +1,9 @@
 extends StaticBody2D
 
+@export var dialogue_resource: JSON
+
 func on_interact():
 	$"../DialogueDisplay".show()
-	$"../DialogueDisplay".start_dialogue(load("res://dialogue/chair.json"))
+	$"../DialogueDisplay".start_dialogue(dialogue_resource)
 	$"../CharacterBody2D".is_interacting = true
 	print("interacting with chair")
