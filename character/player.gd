48e3da5e86c99fa23f011d178a590f50159410de
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2025-07-19 12:00:11 +0200

    fixed white pixel added step sound

--- character/player.gd
+++ character/player.gd
@@ -11,10 +11,15 @@ var interaction_radius = 10
 var is_interacting = false
 var can_interact = true
 
+var step_timer := 0.0
+var step_interval := 0.3  # Time between step sounds, adjust as needed
+
 func _ready():
 	update_animation(Vector2.ZERO)  # Default to idle
 
 func _physics_process(_delta):
+	step_timer -= _delta
+	
 	if !is_interacting:
 		var input_direction = Vector2(
 			Input.get_action_strength("right") - Input.get_action_strength("left"),
@@ -35,6 +40,10 @@ func update_animation(move_input: Vector2):
 	if move_input == Vector2.ZERO:
 		animation_player.pause()
 		return
+	
+	if step_timer <= 0.0:
+		SfXplayer.play()
+		step_timer = step_interval
 
 	if abs(move_input.x) > abs(move_input.y):
 		# Horizontal movement
