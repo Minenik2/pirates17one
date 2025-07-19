a0da24920c0bf7f716e94a7bc4e416a6b09ffec7
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2025-07-19 15:40:39 +0200

    deleted single mother script

--- dialogue/interactEvent.gd
+++ dialogue/interactEvent.gd
@@ -6,4 +6,3 @@ func on_interact():
 	$"../DialogueDisplay".show()
 	$"../DialogueDisplay".start_dialogue(dialogue_resource)
 	$"../CharacterBody2D".is_interacting = true
-	print("interacting with chair")
