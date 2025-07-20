f87cb7ad94b060cb6301ecbc7dd6d4186290cc24
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2025-07-20 13:01:38 +0200

    case journal wip

--- scenes/tab_menu.gd
+++ scenes/tab_menu.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
+@onready var suspects: MarginContainer = $MarginContainer/VBoxContainer/Suspects
+@onready var alibis: MarginContainer = $MarginContainer/VBoxContainer/Alibis
+@onready var victim: MarginContainer = $MarginContainer/VBoxContainer/Victim
+@onready var info: MarginContainer = $MarginContainer/VBoxContainer/Info
+
+
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("tab_menu") and not is_in_main_menu():
 		SfXplayer.playDialogueClick()
@@ -11,3 +17,37 @@ func _unhandled_input(_event: InputEvent) -> void:
 func is_in_main_menu() -> bool:
 	var current_scene = get_tree().current_scene
 	return current_scene.scene_file_path == "res://scenes/main_menu.tscn"
+
+
+func _on_suspects_pressed() -> void:
+	SfXplayer.playDialogueClick()
+	alibis.hide()
+	victim.hide()
+	info.hide()
+	suspects.show()
+	
+
+
+func _on_info_pressed() -> void:
+	SfXplayer.playDialogueClick()
+	suspects.hide()
+	alibis.hide()
+	victim.hide()
+	info.show()
+	
+
+
+func _on_alibis_pressed() -> void:
+	SfXplayer.playDialogueClick()
+	suspects.hide()
+	victim.hide()
+	info.hide()
+	alibis.show()
+
+
+func _on_victim_pressed() -> void:
+	SfXplayer.playDialogueClick()
+	suspects.hide()
+	alibis.hide()
+	info.hide()
+	victim.show()
