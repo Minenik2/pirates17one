cdba62fd07648aae1020a31dd7cd55a82ccd6d95
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2025-07-22 11:23:15 +0200

    tip to press tab and also clue list

--- scenes/database.gd
+++ scenes/database.gd
@@ -2,3 +2,15 @@ extends Node
 
 var teleportCode = ""
 var triggered_flags := {} # triggered events saving across scenes
+
+# to add clue interaction with dialogue state do for example DialogueDisplay.state["clueBloodType"]
+var clueScene = [
+	{"title": "Open Window", "description": "the window was open in the victims room.", "discovered": true}
+]
+var clueBody = [
+	{"title": "Blood Type", "description": "Victims blood type is B-.", "discovered": true},
+	{"title": "Time of Death", "description": "Victim must have died between 7-9 PM yesterday.", "discovered": true}
+]
+var clueOccult = [
+	{"title": "Thorn Paper", "description": "Found in the victims pocket, it shows a symbol of an bleeding eye covered in ink.", "discovered": true}
+]
--- scenes/tab_menu.gd
+++ scenes/tab_menu.gd
@@ -8,13 +8,36 @@ extends CanvasLayer
 @onready var textureSprite: TextureRect = $MarginContainer/VBoxContainer/Suspects/HBoxContainer/VBoxContainer2/TextureRect
 @onready var profile: RichTextLabel = $MarginContainer/VBoxContainer/Suspects/HBoxContainer/VBoxContainer2/MarginContainer/profile
 
+@onready var scene_list: ItemList = $"MarginContainer/VBoxContainer/Victim/HBoxContainer/TabContainer/Crime Scene/MarginContainer/VBoxContainer/sceneList"
+@onready var body_list: ItemList = $"MarginContainer/VBoxContainer/Victim/HBoxContainer/TabContainer/Dead Body/MarginContainer/VBoxContainer/bodyList"
+@onready var occult_list: ItemList = $"MarginContainer/VBoxContainer/Victim/HBoxContainer/TabContainer/???/MarginContainer/VBoxContainer/occultList"
 
+@onready var infoText: RichTextLabel = $MarginContainer/VBoxContainer/Victim/HBoxContainer/PanelContainer4/MarginContainer/VBoxContainer/RichTextLabel
+
+
+func _ready():
+	updateClues()
+	
+func updateClues():
+	for clue in Database.clueScene:
+		if clue["discovered"]:
+			scene_list.add_item(clue["title"])
+	for clue in Database.clueBody:
+		if clue["discovered"]:
+			body_list.add_item(clue["title"])
+	for clue in Database.clueOccult:
+		if clue["discovered"]:
+			occult_list.add_item(clue["title"])
 
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("tab_menu") and not is_in_main_menu():
 		SfXplayer.playDialogueClick()
-		if $".".visible:
+		if $".".visible and !$tabTip.visible:
 			$".".hide()
+		elif $tabTip.visible:
+			$TextureRect.show()
+			$MarginContainer.show()
+			$tabTip.hide()
 		else:
 			$".".show()
 
@@ -68,3 +91,34 @@ func _on_astaroth_pressed() -> void:
 	SfXplayer.playDialogueClick()
 	textureSprite.texture = load("res://art/scholar.png")
 	profile.text = "Mohammed has not yet added a description to this character. Maybe one day when the clock strikes twelve he will lift his pen and inspiration to start commencing the bibliographical narrative."
+
+func addSceneItem():
+	$MarginContainer/VBoxContainer/Victim/HBoxContainer/PanelContainer/MarginContainer/Tree.create_item()
+
+
+func _on_tab_container_tab_changed(tab: int) -> void:
+	SfXplayer.playDialogueClick()
+	match tab:
+		0:	
+			scene_list.deselect_all()
+		1:
+			body_list.deselect_all()
+		2:
+			occult_list.deselect_all()
+
+func _on_occult_list_item_selected(index: int) -> void:
+	var clue = Database.clueOccult[index]
+	infoText.text = clue["description"]
+	SfXplayer.playDialogueClick()
+
+
+func _on_body_list_item_selected(index: int) -> void:
+	var clue = Database.clueBody[index]
+	infoText.text = clue["description"]
+	SfXplayer.playDialogueClick()
+
+
+func _on_scene_list_item_selected(index: int) -> void:
+	var clue = Database.clueScene[index]
+	infoText.text = clue["description"]
+	SfXplayer.playDialogueClick()
